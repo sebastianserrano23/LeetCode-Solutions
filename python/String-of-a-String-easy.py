465b0368b6da8a77cf5896768f5a53d7a2baa8ff
Sebastian Serrano <serranosebastian23@gmail.com>
2023-04-04 15:34:31 -0700

    :wastebasket: adding String-of-a-String-easy.py
    added String-of-a-String-easy.py

---
+++ python/String-of-a-String-easy.py
@@ -0,0 +1,25 @@
+from socket import has_dualstack_ipv6
+
+
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        if needle == "":
+            return 0
+        for i in range(len(haystack) + 1 - len(needle)):
+            for j in range(len(needle)):
+                if haystack[i + j] != needle[j]:
+                    break
+                if j == len(needle) - 1:
+                    return i
+        return -1
+
+# another way to do it 
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        if needle == "":
+            return 0
+        for i in range(len(haystack) + 1 - len(needle)):
+            if haystack[i: i + len(needle)] == needle:
+                return i
+
+        return -1
